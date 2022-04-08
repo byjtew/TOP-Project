c35ccd128bc28244c6861d80dad1b8bd408ac31e
byjtew93 <benjamin.lozes@protonmail.com>
2022-04-08 12:42:12 +0200

    Bugfix: Header global variables + Stable CMake

--- include/lbm_phys.h
+++ include/lbm_phys.h
@@ -6,32 +6,70 @@
 #include "lbm_comm.h"
 
 /********************** CONSTS **********************/
-const int opposite_of[DIRECTIONS];
-const double equil_weight[DIRECTIONS];
-const Vector direction_matrix[DIRECTIONS];
+/**
+ * Poids utilisé pour compenser les différentes de longueur des 9 vecteurs directions.
+**/
+#if DIRECTIONS == 9
+static const double equil_weight[DIRECTIONS] = {
+		4.0 / 9.0,
+		1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0,
+		1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0
+};
+//opposite directions, for bounce back implementation
+static const int opposite_of[DIRECTIONS] = {0, 3, 4, 1, 2, 7, 8, 5, 6};
+/**
+ * Definitions des 9 vecteurs de base utilisé pour discrétiser les directions sur chaque mailles.
+**/
+#if DIMENSIONS == 2
+static const Vector direction_matrix[DIRECTIONS] = {
+		{+0.0, +0.0},
+		{+1.0, +0.0},
+		{+0.0, +1.0},
+		{-1.0, +0.0},
+		{+0.0, -1.0},
+		{+1.0, +1.0},
+		{-1.0, +1.0},
+		{-1.0, -1.0},
+		{+1.0, -1.0}
+};
+#else
+#error Need to defined adapted direction matrix.
+#endif
+#else
+#error Need to defined adapted equibirium distribution function
+#endif
+
 
 /*******************  FUNCTION  *********************/
 //helper
-double get_vect_norme_2(const Vector vect1,const Vector vect2);
+double get_vect_norme_2(const Vector vect1, const Vector vect2);
+
 double get_cell_density(const lbm_mesh_cell_t cell);
-void get_cell_velocity(Vector v,const lbm_mesh_cell_t cell,double cell_density);
-double helper_compute_poiseuille(int i,int size);
+
+void get_cell_velocity(Vector v, const lbm_mesh_cell_t cell, double cell_density);
+
+double helper_compute_poiseuille(int i, int size);
 
 /*******************  FUNCTION  *********************/
 //collistion
-double compute_equilibrium_profile(Vector velocity,double density,int direction);
-void compute_cell_collision(lbm_mesh_cell_t cell_out,const lbm_mesh_cell_t cell_in);
+double compute_equilibrium_profile(Vector velocity, double density, int direction);
+
+void compute_cell_collision(lbm_mesh_cell_t cell_out, const lbm_mesh_cell_t cell_in);
 
 /*******************  FUNCTION  *********************/
 //limit conditions
 void compute_bounce_back(lbm_mesh_cell_t cell);
-void compute_inflow_zou_he_poiseuille_distr( const Mesh *mesh, lbm_mesh_cell_t cell,int id_y);
+
+void compute_inflow_zou_he_poiseuille_distr(const Mesh *mesh, lbm_mesh_cell_t cell, int id_y);
+
 void compute_outflow_zou_he_const_density(lbm_mesh_cell_t mesh);
 
 /*******************  FUNCTION  *********************/
 //main functions
-void special_cells(Mesh * mesh, lbm_mesh_type_t * mesh_type, const lbm_comm_t * mesh_comm);
-void collision(Mesh * mesh_out,const Mesh * mesh_in);
-void propagation(Mesh * mesh_out,const Mesh * mesh_in);
+void special_cells(Mesh *mesh, lbm_mesh_type_t *mesh_type, const lbm_comm_t *mesh_comm);
+
+void collision(Mesh *mesh_out, const Mesh *mesh_in);
+
+void propagation(Mesh *mesh_out, const Mesh *mesh_in);
 
 #endif
--- src/lbm_phys.c
+++ src/lbm_phys.c
@@ -6,36 +6,6 @@
 #include "lbm_phys.h"
 #include "lbm_comm.h"
 
-/********************** CONSTS **********************/
-/**
- * Definitions des 9 vecteurs de base utilisé pour discrétiser les directions sur chaque mailles.
-**/
-#if DIRECTIONS == 9 && DIMENSIONS == 2
-const Vector direction_matrix[DIRECTIONS] = {
-	{+0.0,+0.0},
-	{+1.0,+0.0}, {+0.0,+1.0}, {-1.0,+0.0}, {+0.0,-1.0},
-	{+1.0,+1.0}, {-1.0,+1.0}, {-1.0,-1.0}, {+1.0,-1.0}
-};
-#else
-#error Need to defined adapted direction matrix.
-#endif
-
-/********************** CONSTS **********************/
-/**
- * Poids utilisé pour compenser les différentes de longueur des 9 vecteurs directions.
-**/
-#if DIRECTIONS == 9
-const double equil_weight[DIRECTIONS] = {
-	4.0/9.0 ,
-	1.0/9.0 , 1.0/9.0 , 1.0/9.0 , 1.0/9.0,
-	1.0/36.0, 1.0/36.0, 1.0/36.0, 1.0/36.0
-};
-//opposite directions, for bounce back implementation
-const int opposite_of[DIRECTIONS] = { 0, 3, 4, 1, 2, 7, 8, 5, 6 };
-#else
-#error Need to defined adapted equibirium distribution function
-#endif
-
 /*******************  FUNCTION  *********************/
 /**
  * Renvoie le résultat du produit des deux vecteurs passés en paramêtre.
